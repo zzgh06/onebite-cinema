2deca5e5707e9fb42c2b8e1a0f7ad0ac77793513
zzgh06 <zzgh06@naver.com>
2024-09-10 15:26:01 +0900

    first-commit

---
+++ src/pages/movie/[id].tsx
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+
+export default function Page() {
+  const router = useRouter();
+  const { id } = router.query;
+  return <h1>{id} 영화 상세페이지</h1>;
+}
---
+++ src/pages/search.tsx
@@ -0,0 +1,6 @@
+import { useRouter } from "next/router"
+export default function Page() {
+  const router = useRouter();
+  const { q } = router.query;
+  return <h1>검색결과 : { q }</h1>
+}
